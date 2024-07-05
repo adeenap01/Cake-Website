38b16af58a10edefd48fcfd8e17648387c5b4c31
adeenap01 <adeenapatel01@gmail.com>
2024-07-06 00:45:57 +0530

    updated

---
+++ script.js
@@ -0,0 +1,20 @@
+const dayModeButton = document.querySelector('.day-mode');
+const nightModeButton = document.querySelector('.night-mode');
+const body = document.querySelector('body');
+
+// Function to toggle the dark mode
+function toggleDarkMode() {
+  body.classList.toggle('dark-mode');
+  // Update the button styles based on the current mode
+  if (body.classList.contains('dark-mode')) {
+    dayModeButton.style.display = 'none';
+    nightModeButton.style.display = 'block';
+  } else {
+    dayModeButton.style.display = 'block';
+    nightModeButton.style.display = 'none';
+  }
+}
+
+// Add click event listeners to the buttons
+dayModeButton.addEventListener('click', toggleDarkMode);
+nightModeButton.addEventListener('click', toggleDarkMode);
